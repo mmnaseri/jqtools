a44dabd252f7e010698b54936782b2f3c2aa4140
mmnaseri <m.m.naseri@gmail.com>
2012-11-14 21:18:43 +0330

    Added support for AMD loading to both plugins

--- jquery.id-1.0.js
+++ jquery.id-1.0.js
@@ -6,87 +6,114 @@
  * Released under the MIT license.
  */
 
-(function ($) {
-    /**
-     * This method will generate IDs that are unique across the document and randomly generated
-     * while adding the given prefix.
-     * @param prefix
-     * @return {String}
-     */
-    $.generateId = function (prefix) {
-        if (!prefix) {
-            prefix = "";
-        }
-        var noise = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-', '_'];
-        var id = "";
-        do {
-            var length = Math.ceil(5 + Math.random() * 20);
-            id = prefix;
-            for (var i = 0; i < length; i ++) {
-                if (i < 4 || Math.random() < 0.5) {
-                    id += noise[Math.floor(Math.random() * noise.length)];
-                } else {
-                    id += Math.floor(Math.random() * 10);
-                }
-            }
-        } while (document.getElementById(id));
-        return id;
-    };
-})(jQuery);
+(function () {
+    var definePlugin = function (jQuery) {
 
-(function ($) {
-    /**
-     * This plugin automatically assign unique CSS/HTML identifiers to selected elements.
-     * These identifiers can later be revoked by $.unidentify.
-     * @param prefix String|function that indicates whether a prefix should be added to the generated id
-     * @return {*}
-     */
-    $.fn.identify = function (prefix) {
-        if (!prefix) {
-            prefix = "";
-        }
-        if (!$.isFunction(prefix)) {
-            var value = prefix;
-            prefix = function () {
-                return value;
+        (function ($) {
+            /**
+             * This method will generate IDs that are unique across the document and randomly generated
+             * while adding the given prefix.
+             * @param prefix
+             * @return {String}
+             */
+            $.generateId = function (prefix) {
+                if (!prefix) {
+                    prefix = "";
+                }
+                var noise = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-', '_'];
+                var id = "";
+                do {
+                    var length = Math.ceil(5 + Math.random() * 20);
+                    id = prefix;
+                    for (var i = 0; i < length; i++) {
+                        if (i < 4 || Math.random() < 0.5) {
+                            id += noise[Math.floor(Math.random() * noise.length)];
+                        } else {
+                            id += Math.floor(Math.random() * 10);
+                        }
+                    }
+                } while (document.getElementById(id));
+                return id;
             };
-        }
-        var context = $(this);
-        context.each(function () {
-            var element = $(this);
-            element.trigger("identifying");
-            if (element.get(0).id) {
-                element.trigger("identified");
-                //an ID already exists, no need to add another one.
+        })(jQuery);
+
+        (function ($) {
+            /**
+             * This plugin automatically assign unique CSS/HTML identifiers to selected elements.
+             * These identifiers can later be revoked by $.unidentify.
+             * @param prefix String|function that indicates whether a prefix should be added to the generated id
+             * @return {*}
+             */
+            $.fn.identify = function (prefix) {
+                if (!prefix) {
+                    prefix = "";
+                }
+                if (!$.isFunction(prefix)) {
+                    var value = prefix;
+                    prefix = function () {
+                        return value;
+                    };
+                }
+                var context = $(this);
+                context.each(function () {
+                    var element = $(this);
+                    element.trigger("identifying");
+                    if (element.get(0).id) {
+                        element.trigger("identified");
+                        //an ID already exists, no need to add another one.
+                        return;
+                    }
+                    //we first mark this element as modified by the extension
+                    element.get(0).autoIdentified = true;
+                    element.get(0).setAttribute('id', $.generateId(prefix(element.get(0))));
+                    element.trigger("identified");
+                });
+                return context;
+            }
+        })(jQuery);
+
+        (function ($) {
+            /**
+             * This will clear IDs from any elements that have been modified using $.identify, while
+             * leaving the rest alone.
+             * @return {*}
+             */
+            $.fn.unidentify = function () {
+                var context = $(this);
+                context.each(function () {
+                    var element = $(this);
+                    element.trigger('unidentify');
+                    if (element.get(0).autoIdentified) {
+                        element.get(0).removeAttribute('id');
+                        element.get(0).autoIdentified = null;
+                        element.trigger('unidentified');
+                    }
+                });
+                return context;
+            };
+        })(jQuery);
+
+    };
+
+    if (typeof define === "function" && define.amd) {
+        /**
+         * Registering the plugin as an AMD module if possible.
+         */
+        define('jquery.identify', ['jquery'], function (jQuery) {
+            definePlugin(jQuery);
+        });
+    } else {
+        /**
+         * If not, we will wait for jQuery and the identity plugin to be present first
+         */
+        var interval = setInterval(function () {
+            if (!jQuery || !jQuery.fn) {
                 return;
             }
-            //we first mark this element as modified by the extension
-            element.get(0).autoIdentified = true;
-            element.get(0).setAttribute('id', $.generateId(prefix(element.get(0))));
-            element.trigger("identified");
-        });
-        return context;
+            clearInterval(interval);
+            definePlugin(jQuery);
+        }, 10);
     }
-})(jQuery);
 
-(function ($) {
-    /**
-     * This will clear IDs from any elements that have been modified using $.identify, while
-     * leaving the rest alone.
-     * @return {*}
-     */
-    $.fn.unidentify = function () {
-        var context = $(this);
-        context.each(function () {
-            var element = $(this);
-            element.trigger('unidentify');
-            if (element.get(0).autoIdentified) {
-                element.get(0).removeAttribute('id');
-                element.get(0).autoIdentified = null;
-                element.trigger('unidentified');
-            }
-        });
-        return context;
-    };
-})(jQuery);
+})();
--- jquery.overlay-1.0.js
+++ jquery.overlay-1.0.js
@@ -6,152 +6,181 @@
  * Released under the MIT license.
  */
 
-(function ($) {
-    /**
-     * Generates an overlay covering the selected elements
-     * @param options hash|function
-     *  in case of specifying a function as the options specifier, the function will be passed the
-     *  current element and is expected tp return a hash describing the options
-     *
-     *  Available options include:
-     *
-     *      className       string  the classname used for the overlay object (default: overlay)
-     *      id              string  the unique ID of the overlay object (default: auto generation)
-     *      monitor         boolean whether the size of the overlayed object should be monitored (default: false)
-     *      closeOnClick    boolean whether clicking on the overlay would close it (default: false)
-     *
-     * @return {*}
-     */
-    $.fn.overlay = function (options) {
-        var context = $(this);
-        context.each(function () {
-            if (!options) {
-                options = {};
-            }
-            if (!$.isFunction(options)) {
-                var value = options;
-                options = function () {
-                    return value;
-                };
-            }
-            var element = $(this);
-            var config = options(element.get(0));
-            if (!config) {
-                config = {};
-            }
-            if (!config.className) {
-                config.className = "overlay";
-            }
-            element.trigger('beforeOverlay');
-            element.identify();
-            var id = config.id ? config.id : element.get(0).id + "-overlay";
-            element.trigger("overlaying");
-            element.after("<div class='" + config.className + "' id='" + id + "'></div>");
-            var overlay = $("#" + id);
-            overlay.css('display', 'block');
-            overlay.css('position', 'absolute');
-            overlay.update = function () {
-                var box = {
-                    left:element.offset().left,
-                    top:element.offset().top,
-                    width:element.get(0).offsetWidth,
-                    height:element.get(0).offsetHeight
-                };
-                overlay.css('left', box.left + "px");
-                overlay.css('top', box.top + "px");
-                overlay.css('width', box.width + "px");
-                overlay.css('height', box.height + "px");
-                overlay.trigger('update');
-            };
-            overlay.close = function () {
-                element.trigger('overlayClosing');
-                overlay.trigger('closing');
-                if (config.monitor) {
-                    clearInterval(overlay.get(0).interval);
-                }
-                overlay.remove();
-                element.trigger('overlayClosed');
-                element.unidentify();
-            };
-            element.get(0).overlay = overlay;
-            overlay.update();
-            element.trigger("overlayed");
-            if (config.monitor) {
-                overlay.get(0).interval = setInterval(function () {
-                    overlay.update();
-                }, 1);
-            }
-            if (config.closeOnClick) {
-                overlay.addClass('clickable');
-                overlay.click(function () {
-                    overlay.close();
-                });
-            }
-        });
-        return context;
-    };
-})(jQuery);
+(function () {
 
-(function ($) {
-    /**
-     * This method will add an overlay to the inner rectangle of the window object,
-     * monitoring its size and adjusting to it continually, also allowing for multiple
-     * calls to the overlay function to be handled properly.
-     *
-     * If the overlay function has been called 3 times, then it has to be closed 3 times
-     * for it to truly close
-     * @param options
-     */
-    $.overlay = function (options) {
-        var id = "body-overlay";
-        var bodyOverlay = $("#" + id);
-        var body = $("body").get(0);
-        if (bodyOverlay.length == 0) {
-            $(body.lastChild).after("<div id='" + id + "'></div>");
-            bodyOverlay = $("#" + id);
-            bodyOverlay.get(0).count = 1;
-        } else {
-            bodyOverlay.get(0).count++;
-        }
-        if (bodyOverlay.get(0).count == 1) {
-            if (!options) {
-                options = {};
-            }
-            if ($.isFunction(options)) {
-                options = options.call(body);
-            }
-            var closeOnClick = options.closeOnClick;
-            options.monitor = true;
-            options.closeOnClick = false;
-            bodyOverlay.css('z-index', parseInt(bodyOverlay.css('z-index')) + 1000);
-            function updateSize() {
-                bodyOverlay.css('display', 'block');
-                bodyOverlay.css('position', 'absolute');
-                bodyOverlay.css('left', $(window).scrollLeft());
-                bodyOverlay.css('top', $(window).scrollTop());
-                bodyOverlay.css('width', $(window).width() + "px");
-                bodyOverlay.css('height', $(window).height() + "px");
-            }
-            updateSize();
-            bodyOverlay.on('overlayed',function () {
-                if (closeOnClick) {
-                    bodyOverlay.get(0).overlay.click(function () {
-                        bodyOverlay.get(0).count--;
-                        if (bodyOverlay.get(0).count == 0) {
-                            bodyOverlay.get(0).overlay.close();
-                            clearInterval(bodyOverlay.get(0).interval);
+    var definePlugin = function (jQuery) {
+
+        (function ($) {
+            /**
+             * Generates an overlay covering the selected elements
+             * @param options hash|function
+             *  in case of specifying a function as the options specifier, the function will be passed the
+             *  current element and is expected tp return a hash describing the options
+             *
+             *  Available options include:
+             *
+             *      className       string  the classname used for the overlay object (default: overlay)
+             *      id              string  the unique ID of the overlay object (default: auto generation)
+             *      monitor         boolean whether the size of the overlayed object should be monitored (default: false)
+             *      closeOnClick    boolean whether clicking on the overlay would close it (default: false)
+             *
+             * @return {*}
+             */
+            $.fn.overlay = function (options) {
+                var context = $(this);
+                context.each(function () {
+                    if (!options) {
+                        options = {};
+                    }
+                    if (!$.isFunction(options)) {
+                        var value = options;
+                        options = function () {
+                            return value;
+                        };
+                    }
+                    var element = $(this);
+                    var config = options(element.get(0));
+                    if (!config) {
+                        config = {};
+                    }
+                    if (!config.className) {
+                        config.className = "overlay";
+                    }
+                    element.trigger('beforeOverlay');
+                    element.identify();
+                    var id = config.id ? config.id : element.get(0).id + "-overlay";
+                    element.trigger("overlaying");
+                    element.after("<div class='" + config.className + "' id='" + id + "'></div>");
+                    var overlay = $("#" + id);
+                    overlay.css('display', 'block');
+                    overlay.css('position', 'absolute');
+                    overlay.update = function () {
+                        var box = {
+                            left:element.offset().left,
+                            top:element.offset().top,
+                            width:element.get(0).offsetWidth,
+                            height:element.get(0).offsetHeight
+                        };
+                        overlay.css('left', box.left + "px");
+                        overlay.css('top', box.top + "px");
+                        overlay.css('width', box.width + "px");
+                        overlay.css('height', box.height + "px");
+                        overlay.trigger('update');
+                    };
+                    overlay.close = function () {
+                        element.trigger('overlayClosing');
+                        overlay.trigger('closing');
+                        if (config.monitor) {
+                            clearInterval(overlay.get(0).interval);
                         }
+                        overlay.remove();
+                        element.trigger('overlayClosed');
+                        element.unidentify();
+                    };
+                    element.get(0).overlay = overlay;
+                    overlay.update();
+                    element.trigger("overlayed");
+                    if (config.monitor) {
+                        overlay.get(0).interval = setInterval(function () {
+                            overlay.update();
+                        }, 1);
+                    }
+                    if (config.closeOnClick) {
+                        overlay.addClass('clickable');
+                        overlay.click(function () {
+                            overlay.close();
+                        });
+                    }
+                });
+                return context;
+            };
+        })(jQuery);
+
+        (function ($) {
+            /**
+             * This method will add an overlay to the inner rectangle of the window object,
+             * monitoring its size and adjusting to it continually, also allowing for multiple
+             * calls to the overlay function to be handled properly.
+             *
+             * If the overlay function has been called 3 times, then it has to be closed 3 times
+             * for it to truly close
+             * @param options
+             */
+            $.overlay = function (options) {
+                var id = "body-overlay";
+                var bodyOverlay = $("#" + id);
+                var body = $("body").get(0);
+                if (bodyOverlay.length == 0) {
+                    $(body.lastChild).after("<div id='" + id + "'></div>");
+                    bodyOverlay = $("#" + id);
+                    bodyOverlay.get(0).count = 1;
+                } else {
+                    bodyOverlay.get(0).count++;
+                }
+                if (bodyOverlay.get(0).count == 1) {
+                    if (!options) {
+                        options = {};
+                    }
+                    if ($.isFunction(options)) {
+                        options = options.call(body);
+                    }
+                    var closeOnClick = options.closeOnClick;
+                    options.monitor = true;
+                    options.closeOnClick = false;
+                    bodyOverlay.css('z-index', parseInt(bodyOverlay.css('z-index')) + 1000);
+                    function updateSize() {
+                        bodyOverlay.css('display', 'block');
+                        bodyOverlay.css('position', 'absolute');
+                        bodyOverlay.css('left', $(window).scrollLeft());
+                        bodyOverlay.css('top', $(window).scrollTop());
+                        bodyOverlay.css('width', $(window).width() + "px");
+                        bodyOverlay.css('height', $(window).height() + "px");
+                    }
+
+                    updateSize();
+                    bodyOverlay.on('overlayed',function () {
+                        if (closeOnClick) {
+                            bodyOverlay.get(0).overlay.click(function () {
+                                bodyOverlay.get(0).count--;
+                                if (bodyOverlay.get(0).count == 0) {
+                                    bodyOverlay.get(0).overlay.close();
+                                    clearInterval(bodyOverlay.get(0).interval);
+                                }
+                            });
+                        }
+                    }).on('overlayClosed',function () {
+                            bodyOverlay.remove();
+                        }).overlay(options);
+                    $(window).resize(function () {
+                        updateSize();
+                    });
+                    $(window).scroll(function () {
+                        updateSize();
                     });
                 }
-            }).on('overlayClosed',function () {
-                    bodyOverlay.remove();
-                }).overlay(options);
-            $(window).resize(function () {
-                updateSize();
-            });
-            $(window).scroll(function () {
-                updateSize();
-            });
-        }
+            };
+        })(jQuery);
+
     };
-})(jQuery);
+
+    if (typeof define === "function" && define.amd) {
+        /**
+         * Registering the plugin as an AMD module if possible.
+         */
+        define('jquery.overlay', ['jquery', 'jquery-id'], function (jQuery) {
+            definePlugin(jQuery);
+        });
+    } else {
+        /**
+         * If not, we will wait for jQuery and the identity plugin to be present first
+         */
+        var interval = setInterval(function () {
+            if (!jQuery || !jQuery.fn || !jQuery.fn.identify) {
+                return;
+            }
+            clearInterval(interval);
+            definePlugin(jQuery);
+        }, 10);
+    }
+
+})();
